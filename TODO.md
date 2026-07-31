57684900506659f79f8e7cf585dc12ab897ae157
Alexis Rossi <alexisjrossi@gmail.com>
2026-07-31 00:04:52 -0400

    Adding authentication, form validation and metadata

---
+++ TODO.md
@@ -0,0 +1,1 @@
+1. Add error handling to the edit-form
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
+import { Metadata } from "next";
 
-
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The acme dashboard.',
+  metadataBase: new URL('url-to-metadata'),
+};
 export default function RootLayout({
   children,
 }: {
